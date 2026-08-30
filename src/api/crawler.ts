0b1e5957729e09db38d13045d1108a5a07cd5cc2
Jimkk111 <2444857531@qq.com>
2026-08-30 23:38:21 +0800

    fix: 为爬虫操作设置 2 分钟超时

--- src/api/crawler.ts
+++ src/api/crawler.ts
@@ -12,5 +12,7 @@ export interface CrawlRequest {
 }
 
 export async function crawlNews(data: CrawlRequest): Promise<CrawlResponse> {
-  return post<CrawlResponse>('/crawler/crawl', data)
+  return post<CrawlResponse>('/crawler/crawl', data, {
+    timeout: 120000, // 爬虫操作耗时较长，单独设置 2 分钟超时
+  })
 }
